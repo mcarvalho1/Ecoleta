9c29a560f37a7795730dfce8526a89950daa0bd4
Matheus Carvalho <matheuscarvalho850@gmail.com>
2020-06-05 16:37:00 -0300

    commit

--- src/server.js
+++ src/server.js
@@ -1,6 +1,33 @@
 const express = require("express")
 const server = express()
 
-//ligar o servidor
+// configurar pasta públuca
+server.use(express.static("public"))
 
+
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
+// configurar caminhos da aplicação
+//página inicial
+// req = requisição
+// res = resposta 
+server.get("/", (req, res) => {
+    return res.render("index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search-result", (req, res) => {
+    return res.render("search-result.html")
+})
+
+//ligar o servidor
 server.listen(3000)
