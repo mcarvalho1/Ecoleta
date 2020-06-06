18ac8aa09b8e86bf822eb8daadf239cb5762d4ea
Matheus Carvalho <matheuscarvalho850@gmail.com>
2020-06-06 17:56:08 -0300

    commit

--- src/database/db.js
+++ src/database/db.js
@@ -23,27 +23,27 @@ db.serialize(() => {
      `)
 
     //inserir dados na tabela
-    const query = `
-         INSERT INTO places (
-             image,
-             name,
-             address,
-             address2,
-             state,
-             city,
-             items
-         ) VALUES (?,?,?,?,?,?,?);
-     `
+    // const query = `
+    //      INSERT INTO places (
+    //          image,
+    //          name,
+    //          address,
+    //          address2,
+    //          state,
+    //          city,
+    //          items
+    //      ) VALUES (?,?,?,?,?,?,?);
+    //  `
 
-    const values = [
-        "https://images.unsplash.com/photo-1558583082-409143c794ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80",
-        "Papersider",
-        "Av. Marechal Floriano, Centro",
-        "N° 360",
-        "RJ",
-        "Rio de Janeiro",
-        "Resíduos Orgânicos"
-    ]
+    // const values = [
+    //     "https://images.unsplash.com/photo-1558583082-409143c794ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80",
+    //     "Papersider",
+    //     "Av. Marechal Floriano, Centro",
+    //     "N° 360",
+    //     "RJ",
+    //     "Rio de Janeiro",
+    //     "Resíduos Orgânicos"
+    // ]
 
     function afterInsertData(err) {
         if (err) {
@@ -54,9 +54,9 @@ db.serialize(() => {
         console.log(this)
     }
 
-    db.run(query, values, afterInsertData)
+    //db.run(query, values, afterInsertData)
 
-    //     // //consultar dados na tabela
+    //consultar dados na tabela
     db.all(`SELECT * FROM places`, function (err, rows) {
         if (err) {
             return console.log(err)
@@ -66,12 +66,13 @@ db.serialize(() => {
         console.log(rows)
     })
 
-// //deletar dado da tabela
-// db.run(`DELETE FROM places WHERE id = ?`, [1], function(err){
-//     if(err) {
-//         return console.log(err)
-//     }
+    // //deletar dado da tabela
+    // //Para deletar apenas um ID colocar está linha de código abaixo >  DELETE FROM places WHERE id = ?`, [1]
+    //  db.run(`DELETE FROM places`, [], function(err){
+    //      if(err) {
+    //          return console.log(err)
+    //      }
 
-//     console.log("Registro deletado com sucesso!")
-//     })
- })
+    //      console.log("Registro deletado com sucesso!")
+    //      })
+})
