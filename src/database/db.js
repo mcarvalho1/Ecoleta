b2cb709db427c0bb4b059f77ed61b60a43371a73
Matheus Carvalho <matheuscarvalho850@gmail.com>
2020-06-06 14:45:22 -0300

    commit

--- src/database/db.js
+++ src/database/db.js
@@ -6,12 +6,71 @@ const sqlite3 = require("sqlite3").verbose()
 const db = new sqlite3.Database("./src/database/database.db")
 
 //utilizar o objeto de banco de dados para operações
-db.serialize( () =>{
-    //criar tabela
+// db.serialize( () =>{
+//     //criar tabela com comandos sql
+//     db.run(`
+//         CREATE TABLE IF NOT EXISTS places (
+//             id  INTEGER PRIMARY KEY AUTOINCREMENT,
+//             image TEXT,
+//             name TEXT,
+//             address TEXT,
+//             address2 TEXT,
+//             state TEXT,
+//             city TEXT,
+//             items TEXT
+//         );
+//     `)
 
-    //inserir dados na tabela
+//     //inserir dados na tabela
+//     const query = `
+//         INSERT INTO places (
+//             image,
+//             name,
+//             address,
+//             address2,
+//             state,
+//             city,
+//             items
+//         ) VALUES (?,?,?,?,?,?,?);
+//     `
 
-    //consultar dados na tabela
+//     const values = [
+//         "https://images.unsplash.com/photo-1558583082-409143c794ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80",
+//         "Papersider",
+//         "Av. Marechal Floriano, Centro",
+//         "N° 360",
+//         "RJ",
+//         "Rio de Janeiro",
+//         "Resíduos Orgânicos"
+//     ]
 
-    //deletar dado da tabela
-})
+//     function afterInsertData(err) {
+//         if(err) {
+//             return console.log(err)
+//         }
+
+//         console.log("Cadastrado com sucesso!")
+//         console.log(this)
+//     }
+
+//     db.run(query, values, afterInsertData)
+
+//     // //consultar dados na tabela
+//     // db.all(`SELECT * FROM places`, function(err, rows) {
+//     //     if(err) {
+//     //         return console.log(err)
+//     //     }
+
+//     //     console.log("Aqui estão seus registros")
+//     //     console.log(rows)
+//     // })
+
+//     // //deletar dado da tabela
+//     // db.run(`DELETE FROM places WHERE id = ?`, [1], function(err){
+//     //     if(err) {
+//     //         return console.log(err)
+//     //     }
+
+//     //     console.log("Registro deletado com sucesso!")
+//     // })
+// })
